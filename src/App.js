77b175fae3492fa166dbaade63c6154f32abcedd
shikha <mshikhamaurya1996@gmail.com>
2024-04-03 17:16:03 +0530

    created new project of todo list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import AddToDo from "./components/AddToDo";
+import ToDoList from "./components/ToDoList";
+import { Provider } from "react-redux";
+import Appstore from "./store/AppStore";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="h-screen">
+    <Provider store={Appstore}> 
+    <AddToDo/>
+      <ToDoList/>
+    </Provider>
+    
+      </div>  
   );
 }
 
